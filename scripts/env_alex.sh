2d2c525e0fa1f78aafa5fa60ba663791aca8013f
Mahfuzur Rahman Chowdhury <mahfuzur.rahman.c@gmail.com>
2026-09-07 23:19:08 +0200

    T7 and T8 erros

--- scripts/env_alex.sh
+++ scripts/env_alex.sh
@@ -11,6 +11,7 @@ export STAGE_DIR=/scratch/iwi5359h/imagenet_${SLURM_JOB_ID}
 # ── Conda — must use full path in non-interactive sbatch jobs ─────────────────
 source /etc/profile
 module load python/3.12-conda
+#module load python
 
 # Activate using full path — 'conda activate' does not work in sbatch
 source activate /home/vault/iwi5/iwi5359h/envs/saga
